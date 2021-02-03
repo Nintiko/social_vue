d5fadba0761d13684bb28519c4850c27568bb488
Petr Makarov <mr.ru.20042004@gmail.com>
2021-02-03 12:10:58 +0300

    nav-bar ready

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
 <template>
-  <v-app i="app">
-    <v-navigation-drawer app></v-navigation-drawer>
-    <v-content app></v-content>
+  <v-app id="app">
+    <v-navigation-drawer
+      app
+      color="blue accent-1"
+      dark
+      expand-on-hover
+      permanent
+      right
+    >
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img
+              size="100"
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title"> Sandra Adams </v-list-item-title>
+            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense shaped>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-home-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Главная</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Мой профиль</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-multiple-plus-outline</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Найти друзей</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content class="px-12 py-3" app>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -9,9 +62,7 @@
 export default {
   name: "App",
 
-  components: {
-
-  },
+  components: {},
 
   data: () => ({
     //
